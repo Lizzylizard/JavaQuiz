9b11ac2f94f27a31d07e6a4c61ada7370e2579fd
Lizzylizard <elisabeth.milde@informatik.hs-fulda.de>
2019-12-16 19:14:36 +0200

    Initial commit

---
+++ Quiz/src/quiz/MyFile.java
@@ -0,0 +1,8 @@
+package quiz;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class MyFile {
+
+}
