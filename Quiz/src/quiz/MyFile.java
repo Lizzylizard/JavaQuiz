1dddba46756088d013d994ea57c3f24a95499ce1
Lizzylizard <elisabeth.milde@informatik.hs-fulda.de>
2020-01-07 17:18:30 +0200

    Correct answers are stored in their own array.

--- Quiz/src/quiz/MyFile.java
+++ Quiz/src/quiz/MyFile.java
@@ -33,11 +33,16 @@ public class MyFile {
 		        i++;
 		        this.storeQuestionsAndAnswers();
 		      }
+				
+		      //after file is read, store the correct answers separately in an array
+		      this.storeCorrectAnswers();
+		      
 		      sc.close();
 		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
 		    }
+
 	}
 	
 	/**
@@ -54,6 +59,30 @@ public class MyFile {
 				for(int j = 0; j < strings.length; j++) {
 					//stores the answers as columns of the question matrix
 					questions[i][j] = strings[j];
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Stores the correct answer to a question in correctAnswer-array.
+	 * For example: questions[1][4] holds the correct answer to question 2. Then the value in correctAnswer[1] will be = 4.
+	 */
+	private void storeCorrectAnswers() {
+		for(int i = 0; i < this.questions.length; i++) {
+			String currQues = this.questions[i][0];
+			//check if there is a next question in the questions-array
+			if (currQues != null) {
+				for(int j = 0; j < this.questions[i].length; j++) {
+					//String that holds the answer j to question i 
+					String currAns = this.questions[i][j];
+					//check if there is a next answer to the current question
+					if(currAns != null) {
+						//if the first letter of the answer is an X, it means it is the correct answer -> has to be put in correctAnswer
+						if(currAns.charAt(0) == 'X') {
+							this.correctAnswer[i] = j;
+						}
+					}
 				}
 			}
 		}
--- Quiz/src/quiz/Quiz.java
+++ Quiz/src/quiz/Quiz.java
@@ -4,24 +4,28 @@ public class Quiz {
 	MyFile file = new MyFile();
 	
 	/**
-	 * Shows all questions stored in the questions matrix.
+	 * Shows all questions stored in the questions matrix and their correct answers.
 	 * Just for testing purposes.
 	 */
 	public void showQuestions() {
 		String[][] questions = file.getQuestions();
+		int[] answers = file.getAnswers();
 		for(int i = 0; i < questions.length; i++) {
 			for(int j = 0; j < questions[i].length; j++) {
 				if(questions[i][j] != null) {
 					System.out.println(questions[i][j]);
 				}
 			}
+			if(answers[i] != 0) {
+				System.out.println("Correct answer is: " + answers[i] + "\n");
+			}
 		}
 	}
 
 	public static void main(String[] args) {
-		/*Quiz myQuiz = new Quiz();
+		Quiz myQuiz = new Quiz();
 		myQuiz.file.readQuestions();
-		myQuiz.showQuestions();*/
+		myQuiz.showQuestions();
 	}
 
 }
